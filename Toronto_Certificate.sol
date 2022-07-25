1c9daee2a0a8231379eda65c3134ce1225a9db1e
"Satish Surath: RemixIDE" <"s@n-m.co">
2022-07-25 18:47:20 -0400

    Inital Upload of the Toronto_Certificate.sol

---
+++ Toronto_Certificate.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: Univeristy of Toronto
+pragma solidity ^0.8.1;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";    
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
