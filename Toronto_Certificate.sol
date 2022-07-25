80ec514706df6d85a43c5f2cb6c038589edc9817
"Satish Surath: RemixIDE" <"s@n-m.co">
2022-07-25 18:51:18 -0400

    Creating the Smart Contract in Solidity

--- Toronto_Certificate.sol
+++ Toronto_Certificate.sol
@@ -4,3 +4,8 @@ pragma solidity ^0.8.1;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";    
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract Toronto_Certificate is ERC721Enumerable, Ownable {
+    using Strings for uint256;
+
+}
